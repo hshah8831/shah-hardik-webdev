5368a5afab7b8f62ecb0224e1f92577a633779c9
hshah8831 <hshah8831@gmail.com>
2017-03-19 17:10:04 -0400

    fixed flickr functionality

--- public/assignment/services/flickr.service.client.js
+++ public/assignment/services/flickr.service.client.js
@@ -1,8 +1,22 @@
-var key = "77d58defefc2d89966beb965154e5571";
-var secret = "7b61b923452810ea";
-var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
+(function() {
+    angular
+        .module("WebAppMaker")
+        .factory('FlickrService', flickrService);
 
-function searchPhotos(searchTerm) {
-    var url = urlBase.replace("API_KEY", key).replace("TEXT", searchTerm);
-    return $http.get(url);
-}
+    function flickrService($http) {
+        var key = "77d58defefc2d89966beb965154e5571";
+        var secret = "7b61b923452810ea";
+        var urlBase = "https://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&api_key=API_KEY&text=TEXT";
+
+        var api = {
+            "searchPhotos": searchPhotos
+        };
+        return api;
+
+        function searchPhotos(searchTerm) {
+            var url = urlBase.replace("API_KEY", key).replace("TEXT", searchTerm);
+            return $http.get(url);
+        }
+    }
+
+})();
--- public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
+++ public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
@@ -3,9 +3,17 @@
         .module("WebAppMaker")
         .controller("widgetFlickrSearch", widgetFlickrSearch);
 
-    function widgetEditController(FlickrService) {
+    function widgetFlickrSearch($routeParams, $location, FlickrService, WidgetService) {
         var vm = this;
-        vm.searchPhotos = function(searchTerm) {
+        vm.searchPhotos = searchPhotos
+        vm.selectPhoto = selectPhoto;
+
+        vm.userId = $routeParams.uid;
+        vm.websiteId = $routeParams.wid;
+        vm.pageId = $routeParams.pid;
+        vm.widgetId = $routeParams.wgid;
+
+        function searchPhotos(searchTerm) {
             FlickrService
                 .searchPhotos(searchTerm)
                 .then(function(response) {
@@ -13,6 +21,32 @@
                     data = data.substring(0,data.length - 1);
                     data = JSON.parse(data);
                     vm.photos = data.photos;
+                }, function (err) {
+                    vm.error = err;
+                });
+        }
+
+        function selectPhoto(photo){
+            var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
+            url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
+            WidgetService
+                .findWidgetById(vm.widgetId)
+                .then(function (widget) {
+                    if(widget.image){
+                        widget.image.url = url;
+                    } else {
+                        widget.image = {};
+                        widget.image.url = url;
+                    }
+                    return WidgetService
+                        .updateWidget(vm.widgetId, widget);
+                },function (err) {
+                    vm.error = err;
+                }).then(function (result) {
+                    var dest = "/user/"+vm.userId+"/website/"+vm.websiteId+"/page/"+ vm.pageId+ "/widget/" + vm.widgetId;
+                    $location.url(dest);
+                }, function (err) {
+                    vm.error = err;
                 });
         }
     }
